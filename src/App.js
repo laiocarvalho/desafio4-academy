20b2e6765e277c68ec521583497229295d948784
Laio Carvalho <laiocarvalho0@gmail.com>
2021-04-29 17:51:51 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import './App.css';
-import './Assets/Sass/main.css';
-import ForgotPassword from './Pages/ForgotPasswordPage/ForgotPassword';
-import Login from './Pages/LoginPage/Login';
-import Signup from './Pages/SignupPage/Signup';
-
 function App() {
-  return (
-    <div className="App">
-      <ForgotPassword />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
